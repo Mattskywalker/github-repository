2692004c3f37c8d48f71f2ff558fb54d15c8203c
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2022-04-24 17:45:31 -0300

    interface

---
+++ src/interfaces/IRepositories.ts
@@ -0,0 +1,12 @@
+
+
+export interface IRepository {
+    id: number,
+    name: string,
+    full_name: string,
+    description: string,
+    html_url: string,
+    branches_url?: string,
+    issues_url?: string,
+}
+
